10734fc3b84f30323ec67e2d36566f67157b8c67
Daniel Echeverry Giraldo <dfecheverryg@unal.edu.co>
2018-01-26 07:16:39 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -5,50 +5,50 @@ class TicTacToe
   end
 
   WIN_COMBINATIONS = [
-  [0,1,2],
-  [3,4,5],
-  [6,7,8],
-  [0,3,6],
-  [1,4,7],
-  [2,5,8],
-  [0,4,8],
-  [2,4,6]
-]
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ]
 
   def display_board
-  puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-  puts "-----------"
-  puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-  puts "-----------"
-  puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
 
   def input_to_index(user_input)
-  user_input.to_i - 1
+    user_input.to_i - 1
   end
 
   def move(index, current_player)
-  @board[index] = current_player
+    @board[index] = current_player
   end
 
   def position_taken?(index)
-  !(@board[index].nil? || @board[index] == " ")
+    !(@board[index].nil? || @board[index] == " ")
   end
 
   def valid_move?(index)
-  index.between?(0,8) && !position_taken?(index)
+    index.between?(0,8) && !position_taken?(index)
   end
 
   def turn(board)
     puts "Please enter 1-9:"
     input = gets.strip
     index = input_to_index(input)
-  if valid_move?(board, index)
-    move(board, index, current_player(board))
-    display_board(board)
-  else
-    turn(board)
-  end
+    if valid_move?(board, index)
+      move(board, index, current_player(board))
+      display_board(board)
+    else
+      turn(board)
+    end
   end
 
 
