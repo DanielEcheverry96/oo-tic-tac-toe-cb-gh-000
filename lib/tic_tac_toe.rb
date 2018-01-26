eac9658f69ec0e6d0e02069f3f0366133e5ea30a
Daniel Echeverry Giraldo <dfecheverryg@unal.edu.co>
2018-01-26 07:19:18 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -51,5 +51,18 @@ class TicTacToe
     end
   end
 
+  #def turn_count(board)
+    #counter = 0
+    #board.each do |index|
+      #if index == "X" || index == "O"
+        #counter += 1
+      #end
+    #end
+    #counter
+  #end
 
+
+def turn_count
+   @board.count{|token| token == "X" || token == "O"}
+ end
 end
